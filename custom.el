7a5876f76d902d57fb4f5ffdf48ee164dcd9126a
Bryson Oakley <broaken13@gmail.com>
2018-08-02 21:11:30 -0600

    Put config in an org file

---
+++ custom.el
@@ -0,0 +1,20 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(company-quickhelp-color-background "midnight blue")
+ '(company-quickhelp-color-foreground "light gray")
+ '(company-quickhelp-mode t)
+ '(custom-safe-themes
+   (quote
+    ("6c7db7fdf356cf6bde4236248b17b129624d397a8e662cf1264e41dab87a4a9a" default)))
+ '(package-selected-packages
+   (quote
+    (company-jedi color-identifiers-mode undo-tree try use-package yoshi-theme xref-js2 which-key web-mode tide smartparens py-autopep8 omnisharp material-theme magit js2-refactor impatient-mode helm emmet-mode elpy company-tern company-quickhelp better-defaults ace-window ac-js2))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
