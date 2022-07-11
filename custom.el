516abfdab582cf72eedd5113a6850b61701b2e39
Bryson Oakley <broaken13@gmail.com>
2022-07-10 20:54:34 -0600

    Removed company-tern
    It doesn't seem to be a thing anymore

--- custom.el
+++ custom.el
@@ -7,11 +7,9 @@
  '(company-quickhelp-color-foreground "light gray")
  '(company-quickhelp-mode t)
  '(custom-safe-themes
-   (quote
-    ("6c7db7fdf356cf6bde4236248b17b129624d397a8e662cf1264e41dab87a4a9a" default)))
+   '("cafebef22e58ed5b47b1afb8fd3a4deb26a8d6addcd297cc3e8b415ceb8a17a6" "6c7db7fdf356cf6bde4236248b17b129624d397a8e662cf1264e41dab87a4a9a" default))
  '(package-selected-packages
-   (quote
-    (helm-projectile projectile-helm projectile ox-twbs org-bullets company-jedi color-identifiers-mode undo-tree try use-package yoshi-theme xref-js2 which-key web-mode tide smartparens py-autopep8 omnisharp material-theme magit js2-refactor impatient-mode helm emmet-mode elpy company-tern company-quickhelp better-defaults ace-window ac-js2))))
+   '(js2-mode flycheck yasnippet company helm-projectile projectile-helm projectile ox-twbs org-bullets company-jedi color-identifiers-mode undo-tree try use-package yoshi-theme xref-js2 which-key web-mode tide smartparens py-autopep8 omnisharp material-theme magit js2-refactor impatient-mode helm emmet-mode elpy company-tern company-quickhelp better-defaults ace-window ac-js2)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
