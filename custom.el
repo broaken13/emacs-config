051d08d648cfafea0017a5a977f6d84188974989
Bryson Oakley <broaken13@gmail.com>
2023-01-17 14:46:39 -0700

    Redo of config.org

--- custom.el
+++ custom.el
@@ -7,9 +7,9 @@
  '(company-quickhelp-color-foreground "light gray")
  '(company-quickhelp-mode t)
  '(custom-safe-themes
-   '("cafebef22e58ed5b47b1afb8fd3a4deb26a8d6addcd297cc3e8b415ceb8a17a6" "6c7db7fdf356cf6bde4236248b17b129624d397a8e662cf1264e41dab87a4a9a" default))
+   '("7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "7e377879cbd60c66b88e51fad480b3ab18d60847f31c435f15f5df18bdb18184" "cafebef22e58ed5b47b1afb8fd3a4deb26a8d6addcd297cc3e8b415ceb8a17a6" "6c7db7fdf356cf6bde4236248b17b129624d397a8e662cf1264e41dab87a4a9a" default))
  '(package-selected-packages
-   '(js2-mode flycheck yasnippet company helm-projectile projectile-helm projectile ox-twbs org-bullets company-jedi color-identifiers-mode undo-tree try use-package yoshi-theme xref-js2 which-key web-mode tide smartparens py-autopep8 omnisharp material-theme magit js2-refactor impatient-mode helm emmet-mode elpy company-tern company-quickhelp better-defaults ace-window ac-js2)))
+   '(lsp-haskell smartparens-config company-box company-mode helm-lsp lsp-ui lsp-mode doom-modeline doom-themes all-the-icons swiper-helm swiper diminish js2-mode flycheck yasnippet company helm-projectile projectile-helm projectile ox-twbs org-bullets company-jedi color-identifiers-mode undo-tree try use-package yoshi-theme xref-js2 which-key web-mode tide smartparens py-autopep8 omnisharp material-theme magit js2-refactor impatient-mode helm emmet-mode elpy company-tern company-quickhelp better-defaults ace-window ac-js2)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -1,20 +1,7 @@
-;; Set garbage collection threshold
-(setq gc-cons-threshold 50000000)
-;; Fixes some TLS warnings
+;; These help with performance and TLS issues
+(setq gc-cons-threshold 100000000) ;; 100MB
 (setq gnutls-min-prime-bits 4096)
 
-(require 'package)
-(setq package-archives '(("org" . "http://orgmode.org/elpa/")
-                         ("gnu" . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "http://melpa.org/packages/")
-                         ("marmalade" . "http://marmalade-repo.org/packages/")))
-(package-initialize)
-
-
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;;;;;;;;
+(add-to-list 'default-frame-alist '(fullscreen . maximized)) ;; start new windows maximized
 
 (org-babel-load-file (expand-file-name "~/.emacs.d/config.org"))
